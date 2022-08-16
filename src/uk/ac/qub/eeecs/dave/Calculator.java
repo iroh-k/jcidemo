fdab1da25342b11a62ada45a9f854e9f2ad9da4a
Kieran <kieranclifton94@gmail.com>
2022-08-16 11:06:43 +0100

    Added Divde method and updated add test

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -9,4 +9,9 @@ public class Calculator {
 		return a-b;
 	} 
 	
+	public double divide(double a, double b) {
+		return a/b;
+		
+	}
+	
 }
--- src/uk/ac/qub/eeecs/dave/CalculatorTest.java
+++ src/uk/ac/qub/eeecs/dave/CalculatorTest.java
@@ -9,12 +9,18 @@ class CalculatorTest {
 	@Test
 	void testAdd() {
 		Calculator c = new Calculator();
-		assertEquals(c.add(10,1), 11);
+		assertEquals(c.add(10,1), 12);
 	}
 	
 	@Test
 	void testSub() {
 		Calculator c = new Calculator();
 		assertEquals(c.sub(10,1), 9);
+	}
+	
+	@Test 
+	void testDivide() {
+		Calculator c = new Calculator();
+		assertEquals(c.divide(10, 2), 5);
 	}
 }
